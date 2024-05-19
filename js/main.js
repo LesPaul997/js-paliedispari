a7f63ef9a7007a3ea608bececcb2ddfbdddc44e4
LesPaul997 <mariogiordano941@gmail.com>
2024-05-19 15:37:40 +0200

    add : function random

--- js/main.js
+++ js/main.js
@@ -45,3 +45,19 @@ function invertiParola(str) {
 
 
 //SECONDO PROGRAMMA
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+const userChoice = prompt('Scrivi pari o dispari');
+const userNumber = prompt('Scegli un numero da 1 a 5');
+
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+for (let i = 0; i < 1; i++) {
+    const currentNumber = getRndIndecer(1,5);
+    console.log(currentNumber);
+}
+function getRndIndecer (min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Sommiamo i due numeri
